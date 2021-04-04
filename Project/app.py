4e76cc57e966aaaec2acccf45e02a7bd119ce9c7
Mona McElroy <mona.mcelroy@gmail.com>
2021-04-04 12:29:57 -0500

    adding all files

---
+++ Project/app.py
@@ -0,0 +1,209 @@
+# from flask import Flask
+# # resource: https://www.youtube.com/watch?v=Z1RJmh_OqeA
+
+# app = Flask(__name__) #referencing this file
+
+# @app.route('/') #pass in url here; index route so no 404
+# # define function for that route
+# def index():
+#     return "Hello,WIRE powderless!"
+
+# if __name__ == "__main__":
+#     app.run(debug=True) # error would display on webpage
+# #type <localhost:5000> in browser
+from flask import Flask, jsonify, request, session, send_from_directory, send_file
+import flask
+from flask_cors import CORS
+from data_service import hardware, user, dataset
+from passlib.hash import pbkdf2_sha256
+from bson import ObjectId
+import io
+import os
+import shutil
+
+
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+app.secret_key = "secret"
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['snippets'] = os.path.dirname(os.path.abspath(__file__))+'/snippets/'
+
+
+@app.route('/hardware')
+def send_hardware_info():
+    data_list = hardware.get_HWSet_collection_info()
+    # return jsonify({'result': hardware.get_hardware_availability("test1")})
+    data_dict = {}
+    HW_name = []
+    HW_ava = []
+    HW_cap = []
+    for index in range(len(data_list)):
+        HW_name.append(data_list[index]['HWSet_name'])
+        HW_ava.append(data_list[index]['availability'])
+        HW_cap.append(data_list[index]['capacity'])
+    data_dict['HW_name'] = HW_name
+    data_dict['HW_ava'] = HW_ava
+    data_dict['HW_cap'] = HW_cap
+    return jsonify(data_dict)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    # GET
+    if request.method == 'GET':
+        if "user" in session:
+            return jsonify({
+                'ans': 'Y',
+                'userID': session['user']
+            })
+        return jsonify(
+            {
+                'ans': 'N',
+            }
+        )
+
+    # POST
+    data = request.json
+    # print(data)
+    if not data:
+        return jsonify({'message': 'Null request'})
+    if data:
+        username = data['username']
+        password = data['password']
+        # compare user data in MongoDB
+        client = user.get_user(username)
+        # print(client)
+        if client == -1:
+            return jsonify({'message': 'Username does not exist'})
+
+        if not pbkdf2_sha256.verify(password, client['password']):
+            return jsonify({'message': 'Username and password does not match'})
+
+        # # success login, send userID back
+
+        session['user'] = str(client['_id'])  # make jsonID serializable
+        # print(client)
+        if "user" in session:
+            response = jsonify(
+                {'message': 'success', 'userID': session['user']})
+
+            return response
+        return jsonify({'message': 'something wrong'})
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    # GET:
+    if request.method == 'GET':
+        return jsonify({"message": "This will not happen"})
+    # POST:
+    data = request.json
+    if not data:
+        return jsonify({'message': 'Null request'})
+    # data is valid:
+    username = data['username']
+    en_password = pbkdf2_sha256.encrypt(data['password'])
+    email = data['email']
+    res = user.create_user(username, en_password, email)
+    if res[0] == -1:
+        return jsonify({'message': 'Username already exists'})
+    if res[1] == -1:
+        return jsonify({'message': 'Email address already exists'})
+    # success register, send userID back
+    client = user.get_user(username)
+    session['user'] = str(client['_id'])
+    response = jsonify({
+        'message': 'success',
+        'userID': session['user']
+    })
+    return response
+
+
+@app.route('/logout', methods=['POST', 'GET'])
+def logout():
+    if request.method == 'POST':
+        if "user" in session:
+            session.clear()
+            return jsonify({'message': 'logout successfully'})
+        else:
+            return jsonify({'message': 'not logged in'})
+    return jsonify({'message': 'get request will not be handled'})
+
+
+@app.route('/userProfile', methods=['POST', 'GET'])
+def userProfile():
+    # GET:
+    if request.method == 'GET':
+        if 'user' in session:
+            user_id = session['user']
+            client = user.get_user_byID(user_id)
+            return jsonify(
+                {"message": "success", "username": client['username'], "email": client['email']})
+        else:
+            return jsonify({'message': 'not login'})
+    # POST:
+    data = request.json
+    if not data:
+        return jsonify({'message': 'Null request'})
+    # data is valid:
+    print(data)
+    username = data['username']
+    email = data['email']
+    user_id = session['user']
+    res = user.update_user(username, "", email, user_id)
+    if res[0] == -1:
+        return jsonify({'message': 'Username already exists'})
+    if res[1] == -1:
+        return jsonify({'message': 'Email address already exists'})
+    # successfully edited profile, send 'success' back
+    # client = user.get_user(username)
+    response = jsonify({
+        'message': 'success',
+    })
+    return response
+
+#Returns the names of each dataset for display
+@app.route('/dataset_names', methods=['GET', 'POST'])
+def datasetNames():
+    # GET:
+    if request.method == 'GET':
+        data_names = dataset.getDatasetNames()
+        return jsonify(data_names)
+    
+#returns the keys to each Dataset
+@app.route('/dataset_titles', methods=['GET', 'POST'])
+def datasetTitles():
+    # GET:
+    if request.method == 'GET':
+        data_titles = dataset.getDatasetTitles()
+        return jsonify(data_titles)
+
+#Handles zip download
+@app.route('/uploads', methods=['GET', 'POST'])
+def download():
+    if request.method == 'POST':
+        file_name = request.form['filepath']
+        if dataset.getZip(file_name) == True:
+            #grabs zip from snippets folder
+            zip = send_from_directory(directory=app.config['snippets'], filename=file_name, as_attachment=True)
+            #deletes data folder and data zip from /snippets
+            partial_path = app.config['snippets']
+            data_folder_path = partial_path+file_name[:-4]
+            zip_path = partial_path+file_name
+            shutil.rmtree(data_folder_path)
+            #zip.close()
+            os.remove(zip_path)
+            #returns the saved zip file
+            return zip
+    return "hello"
+    
+
+@app.after_request
+def creds(response):
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    return response
+
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
